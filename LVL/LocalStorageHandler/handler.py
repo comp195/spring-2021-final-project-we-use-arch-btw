b048d4ee982e0511d00a8a98fb82b204f769d638
Alex <alexreynen@gmail.com>
2021-03-07 19:37:10 -0800

    Add temp test thing in handler.py

--- LVL/LocalStorageHandler/handler.py
+++ LVL/LocalStorageHandler/handler.py
@@ -39,3 +39,13 @@ class LocalStorageHandler:
     def updateInDB(self):
         # Some kind of code to say find item based on id, then update the needed thing
         pass
+
+if __name__ == "__main__":
+    a = LocalStorageHandler("test.db")
+    # In the real deal, better way of implementing check
+    if not os.path.isfile("test.db"):
+        a.create()
+    # Needed to import state here, but not worth cause its such a pain, thus the plaintext
+    p1 = "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe."
+    a.saveToDB("tt4154796", "Avengers: Endgame", "2019", "PG-13", "Action, Adventure, Drama, Sci-Fi",
+               p1, None, "94%", "/home/user/Desktop/avengers.mp4", "181 min", 'WATCHED', 2)
